070bcecf75cda7431edd84187e2a4660635c6036
sadbc1235 <sadbc1235@naver.com>
2025-03-04 00:57:07 +0900

    모바일 테스트 2

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         , createDt: '2025-02-15'
         , imgUrl: '/profile.webp'
         , title: '프로젝트 이름1'
-        // , content: '프로젝트 내용1asasdfasdfasdfasdfasdjkl;asdf;jkldasfj;lkdsfalj;ksdafjlk;sdfajklsdfaiojhuiguihpgqerbungippuoanguviupqerbngqbuipgquipqhghupbnqvunpbqipubuhiprgeqpongnoqpuniqrpguirhpguhiqpgernpubqrqpuiguhpoqehiropghjioakljn;sdgandksldgjioqeiohgiohqoijrhjiongrqoi[grijo[프로젝트 내용1asasdfasdfasdfasdfasdjkl;asdf;jkldasfj;lkdsfalj;ksdafjlk;sdfajklsdfaiojhuiguihpgqerbungippuoanguviupqerbngqbuipgquipqhghupbnqvunpbqipubuhiprgeqpongnoqpuniqrpguirhpguhiqpgernpubqrqpuiguhpoqehiropghjioakljn;sdgandksldgjioqeiohgiohqoijrhjiongrqoi[grijo[프로젝트 내용1asasdfasdfasdfasdfasdjkl;asdf;jkldasfj;lkdsfalj;ksdafjlk;sdfajklsdfaiojhuiguihpgqerbungippuoanguviupqerbngqbuipgquipqhghupbnqvunpbqipubuhiprgeqpongnoqpuniqrpguirhpguhiqpgernpubqrqpuiguhpoqehiropghjioakljn;sdgandksldgjioqeiohgiohqoijrhjiongrqoi[grijo['
-        , content: '프로젝트 내용1'
+        , content: '프로젝트 내용1asasdfasdfasdfasdfasdjkl;asdf;jkldasfj;lkdsfalj;ksdafjlk;sdfajklsdfaiojhuiguihpgqerbungippuoanguviupqerbngqbuipgquipqhghupbnqvunpbqipubuhiprgeqpongnoqpuniqrpguirhpguhiqpgernpubqrqpuiguhpoqehiropghjioakljn;sdgandksldgjioqeiohgiohqoijrhjiongrqoi[grijo[프로젝트 내용1asasdfasdfasdfasdfasdjkl;asdf;jkldasfj;lkdsfalj;ksdafjlk;sdfajklsdfaiojhuiguihpgqerbungippuoanguviupqerbngqbuipgquipqhghupbnqvunpbqipubuhiprgeqpongnoqpuniqrpguirhpguhiqpgernpubqrqpuiguhpoqehiropghjioakljn;sdgandksldgjioqeiohgiohqoijrhjiongrqoi[grijo[프로젝트 내용1asasdfasdfasdfasdfasdjkl;asdf;jkldasfj;lkdsfalj;ksdafjlk;sdfajklsdfaiojhuiguihpgqerbungippuoanguviupqerbngqbuipgquipqhghupbnqvunpbqipubuhiprgeqpongnoqpuniqrpguirhpguhiqpgernpubqrqpuiguhpoqehiropghjioakljn;sdgandksldgjioqeiohgiohqoijrhjiongrqoi[grijo['
+        // , content: '프로젝트 내용1'
         , views: '10'
     }
     , {
@@ -69,20 +69,20 @@ export default function Home() {
                   <SwiperSlide 
                     key={idx}
                   >
-                    <div
-                      className="h-[650px] px-4 py-2 max-sm:h-[450px]"
-                    >
+                    <div className="h-[500px]">
                       <div
-                        className="w-full h-full rounded-xl flex overflow-hidden"
-                        style={{boxShadow: '1px 1px 10px 1px #00000020'}}
+                        className="w-full h-full rounded-xl flex overflow-hidden max-sm:block"
                       >
                         <div
-                          className="w-1/2"
+                          className="w-1/2 max-sm:w-full max-sm:h-4/6"
                         >
-                          <div className="w-full h-[50px] bg-[#EFF8FF] pl-5 flex items-center text-sm">
+                          <div 
+                            className="w-full h-[50px] bg-[#EFF8FF] pl-5 flex items-center text-sm"
+                          >
                             Created at : {item.createDt}
                           </div>
-                          <div className="h-[calc(100%-50px)] overflow-hidden border-[#e3e3e3] border-b-[1px] border-t-[1px]"
+                          <div 
+                            className="h-[calc(100%-50px)] overflow-hidden border-[#e3e3e3] border-b-[1px] border-t-[1px]"
                           >
                             <img 
                               className="w-full h-full object-cover"
@@ -91,14 +91,14 @@ export default function Home() {
                           </div>
                         </div>
                         <div
-                          className="w-1/2"
+                          className="w-1/2 max-sm:w-full max-sm:h-2/6"
                           style={{boxShadow: '-1px 0px 10px 1px #00000020'}}
                         >
                           <div className='w-full h-full bg-[#fff] p-4 relative'>
                             <h2 className='text-lg line-clamp-1 text-ellipsis break-all'>
                               {item.title}
                             </h2>
-                            <p className='text-sm/6 mt-4 text-wrap whitespace-pre break-all text-ellipsis line-clamp-[20] max-sm:line-clamp-[12]'>
+                            <p className='text-sm/6 mt-4 text-wrap whitespace-pre break-all text-ellipsis line-clamp-[16] max-sm:line-clamp-[2]'>
                               {item.content}
                             </p>
                             <div className='absolute bottom-4 right-4 flex gap-2'>
